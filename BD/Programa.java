03b8a6a47d1cf843345c17e371363196c33dc33b
Dx3r <90462393+RicardoChambel@users.noreply.github.com>
2024-07-12 10:35:33 +0100

    Update Programa.java

--- BD/Programa.java
+++ BD/Programa.java
@@ -3,7 +3,6 @@ package BD;
 import java.nio.file.*;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 import java.io.IOException;
 import java.util.stream.Collectors;
